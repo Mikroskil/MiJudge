18c0f14ae698ac3f99d99d0c4e98071cf6a71ffe
Fredy Ong Jaya <fredy.ong_jaya@yahoo.com>
2013-11-01 17:13:23 +0700

    Mulai pindah ke PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Mikroskil Online Judge</title>
+	<link rel="stylesheet" type="text/css" href="css/global.css">
+	<link rel="stylesheet" type="text/css" href="css/link.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	</head>
+	<body>
+		<div id="page" style="margin: 0 auto; max-width:1200px; min-width:800px;">
+			<?php include_once 'header.html'; ?>
+			<?php include_once 'menu.html'; ?>
+			<div class="clear"></div>
+			<?php include_once 'footer.html'; ?>
+		</div>
+	</body>
+</html>
