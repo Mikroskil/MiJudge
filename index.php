8eaa285ff1646a3d937a14f58a5632153fe70eef
Fredy Ong Jaya <fredy.ong_jaya@yahoo.com>
2013-11-06 17:16:02 +0700

    Ubah jadi php, baru index, header, footer, sidebar sedikit

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Mikroskil Online Judge</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/global.css">
 	<link rel="stylesheet" type="text/css" href="css/link.css">
 	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
 	</head>
 	<body>
-		<div id="page" style="margin: 0 auto; max-width:1200px; min-width:800px;">
+		<div class="page">
 			<?php include_once 'header.html'; ?>
 			<?php include_once 'menu.html'; ?>
+			<?php include_once 'sidebar.html'; ?>
 			<div class="clear"></div>
 			<?php include_once 'footer.html'; ?>
 		</div>
