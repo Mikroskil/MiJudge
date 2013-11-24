5f27741d69fd1374e447d36e1463b3df1fec7ab0
inxidious <shersonwatson@yahoo.com>
2013-11-24 14:23:47 +0700

    tambah form register
    belum ada tambah ke database

--- register/page.php
+++ register/page.php
@@ -1,7 +1,39 @@
 <?php
 	echo "
 <div class='span8'>
-	form register
+	<!--form register-->
+</div>
+<div class='span8'>
+	<form method='post'>
+		<fieldset>
+			<legend>Register</legend>
+			<div class='input-control text' data-role='input-control'>
+				<input type='text' placeholder='First Name' name='firstname'/>
+				<button class='btn-clear'></button>
+			</div>
+			<div class='input-control text' data-role='input-control'>
+				<input type='text' placeholder='Last Name' name='lastname'/>
+				<button class='btn-clear'></button>
+			</div>
+			<div class='input-control text' data-role='input-control'>
+				<input type='text' placeholder='Country' name='country'/>
+				<button class='btn-clear'></button>
+			</div>
+			<div class='input-control text' data-role='input-control'>
+				<input type='text' placeholder='Username' name='username'/>
+				<button class='btn-clear'></button>
+			</div>
+			<div class='input-control password' data-role='input-control'>
+				<input type='password' value='' placeholder='Password' name='password'/>
+				<button class='btn-reveal'></button>
+			</div>
+			<div class='input-control password' data-role='input-control'>
+				<input type='password' value='' placeholder='Confirm Password' name='confirmpassword'/>
+				<button class='btn-reveal'></button>
+			</div>
+			<input type='submit' value='Register'>
+		</fieldset>
+	</form>
 </div>
 ";
 ?>
