82bafde41ef2731d325d52dd18c69aedcf1577eb
inxidious <shersonwatson@yahoo.com>
2013-12-06 23:43:08 +0700

    handle username already exists

--- register/register.php
+++ register/register.php
@@ -1,11 +1,12 @@
 <?php
 	$connect = newConnection();
+	$cek = newQuery($connect, 'select * from team where login=:user', 
+						array('user' => $_POST['username']));
+	if($cek->rowCount()>0){
+		echo '<font color="red">Username already in use. Please choose another username.</font>';
+		return;
+	}
 	$res = $connect->prepare("INSERT INTO `domjudge`.`team` (`login`, `name`, `categoryid`, `affilid`, `authtoken`, `enabled`, `members`, `room`, `comments`, `judging_last_started`, `teampage_first_visited`, `hostname`) VALUES (:user, :name, '2', NULL, :pass, '1', NULL, NULL, NULL, NULL, NULL, NULL)");
 	$res->execute(array('name' => $_POST['firstname'].' '.$_POST['lastname'], 'user' => $_POST['username'], 'pass' => md5($_POST['username'].'#'.$_POST['password'])));
-	if ($res->rowCount() > 0) {
-		echo 'your account has been registered successfully';
-	} else {
-		echo 'register failed';
-		$_POST['register'] = false;
-	}
+	echo 'Your account has been registered successfully';
 ?>
